85cb43583e1c68543050a3ffe62fbcbcd51d1dee
masakichi03 <66109299+masakichi03@users.noreply.github.com>
2021-04-02 01:10:54 +0900

    new upload

---
+++ MouseClickTool/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace MouseClickTool
+{
+    partial class AutoClick
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(41, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(54, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "h";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBox2.Location = new System.Drawing.Point(74, 11);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(41, 20);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(115, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "m";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBox3.Location = new System.Drawing.Point(136, 11);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(41, 20);
+            this.comboBox3.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(179, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "s";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(418, 55);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "セット";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "解除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(326, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "繰り返し";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // AutoClick
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 105);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "AutoClick";
+            this.Text = "AutoClick";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MouseClickTool/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyNamespace;
+
+namespace MouseClickTool
+{
+    public partial class AutoClick : Form
+    {
+        public AutoClick()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 5;
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            
+
+            int nWaitTimeMills = 0;
+            nWaitTimeMills = ( ( int.Parse( comboBox1.Text ) * 3600 ) +
+                ( int.Parse( comboBox2.Text ) * 60 ) + int.Parse( comboBox3.Text ) ) * 1000;
+
+            if ( nWaitTimeMills != 0 ) {
+                // タイマー有効
+                timer1.Interval = nWaitTimeMills; //ミリ秒
+                timer1.Enabled = true;
+
+                DateTime dt = DateTime.Now;
+                DateTime dtAfter = dt.AddSeconds( nWaitTimeMills / 1000 );
+                string sText = dtAfter.ToString( "yyyy/MM/dd HH:mm:ss.fff" );
+                string sMessage = String.Format( "セット：{0}時間{1}分{2}秒後({3})", comboBox1.Text, comboBox2.Text, comboBox3.Text, sText );
+                output_message( sMessage );
+            } else {
+                output_message( "エラー：0秒以上指定");
+            }
+        }
+
+        private void button2_Click( object sender, EventArgs e )
+        {
+            // タイマー解除
+            timer1.Enabled = false;
+            output_message( "解除" );
+        }
+
+        private void output_message( string sOutText )
+        {
+            DateTime dt = DateTime.Now;
+            string sText = dt.ToString( "yyyy/MM/dd HH:mm:ss.fff, " ) + sOutText + "\r\n";
+            textBox1.AppendText( sText );
+            // textBox1.Text += sText;
+        }
+
+
+        private void timer1_Tick( object sender, EventArgs e )
+        {
+
+            Win32Point mousePosition = new Win32Point {
+                X = 0,
+                Y = 0
+            };
+
+            // マウスポインタの現在位置を取得する。
+            NativeMethods.GetCursorPos( ref mousePosition );
+
+            // マウスポインタの現在位置でマウスの左ボタンの押し下げ・押し上げを連続で行うためのパラメータを設定する。
+            INPUT[] inputs = new INPUT[] {
+                new INPUT {
+                    type = NativeMethods.INPUT_MOUSE,
+                    ui = new INPUT_UNION {
+                        mouse = new MOUSEINPUT {
+                            dwFlags = NativeMethods.MOUSEEVENTF_LEFTDOWN,
+                            dx = mousePosition.X,
+                            dy = mousePosition.Y,
+                            mouseData = 0,
+                            dwExtraInfo = IntPtr.Zero,
+                            time = 0
+                        }
+                    }
+                },
+                new INPUT {
+                    type = NativeMethods.INPUT_MOUSE,
+                    ui = new INPUT_UNION {
+                        mouse = new MOUSEINPUT {
+                            dwFlags = NativeMethods.MOUSEEVENTF_LEFTUP,
+                            dx = mousePosition.X,
+                            dy = mousePosition.Y,
+                            mouseData = 0,
+                            dwExtraInfo = IntPtr.Zero,
+                            time = 0
+                        }
+                    }
+                }
+            };
+
+            // 設定したパラメータに従ってマウス動作を行う。
+            NativeMethods.SendInput( 2, ref inputs[0], Marshal.SizeOf( inputs[0] ) );
+            output_message( "クリック：実行" );
+
+            // 繰り返しオフの場合タイマー解除
+            if ( checkBox1.Checked == false ){
+                button2_Click( sender ,e );
+            }
+        }
+    }
+}
